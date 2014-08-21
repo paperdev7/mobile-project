3a9f05b35708fda99ad63c775552c6c44b839a47
huang <huang@huangMacBook-Pro.local>
2014-08-21 23:38:42 +0900

    mobile 201408212338

--- www/js/controllers/page02Ctrl.js
+++ www/js/controllers/page02Ctrl.js
@@ -1,75 +1,62 @@
-define(['angular','googleMapLodash','googleMap'], function (angular) {//,'../../app/bower_components/lodash/dist/lodash.underscore.min','../../app/bower_components/angular-google-maps/dist/angular-google-maps.min'
-	return angular.module('starter.page02Ctrl', ['google-maps'])
+define(['angular'], function (angular) {//,'../../app/bower_components/lodash/dist/lodash.underscore.min','../../app/bower_components/angular-google-maps/dist/angular-google-maps.min'
+	return angular.module('starter.page02Ctrl', [])
 	// A simple controller that fetches a list of data from a service
 	.controller('page02Ctrl', function($scope,$stateParams, $ionicLoading, $compile,$timeout,$log) {
 		$scope.menuId = parseInt($stateParams.pid)-1;
-		//##### default로 위치지정
-		$scope.map = {
-				    center: {
-				        latitude: 37.482,
-				        longitude: 126.896
-				    },
-				    zoom: 8
-				};
-		 $scope.marker = {
-					id:0,
-					coords: {
-						latitude: 37.482,
-						longitude: 126.896
-					},
-					options: { draggable: true },
-					events: {
-						dragend: function (marker, eventName, args) {
-							$log.log('marker dragend');
-							$log.log(marker.getPosition().lat());
-							$log.log(marker.getPosition().lng());
-						}
-					}
-				}
-        $scope.options = {scrollwheel: false};
-        //### default로 위치지정
-		//@@@ 현재 위치 획득
-        $scope.getCurrent = function(){
-		 $scope.loading = $ionicLoading.show({
-           content: 'Getting current location...',
-           showBackdrop: false
-         });
-		 //좌표 획득
-		 navigator.geolocation.getCurrentPosition(function(position){
-		 	var latitude = position.coords.latitude;
-		 	var longitude = position.coords.longitude;
-		 	$scope.$apply(function(){
-				$scope.map = {
-							center: {
-								latitude: latitude,
-								longitude: longitude
-							},
-							zoom: 16
-						};
-				
-				 $scope.marker = {
-					id:0,
-					coords: {
-						latitude: latitude,
-						longitude: longitude
-					},
-					options: { draggable: true },
-					events: {
-						dragend: function (marker, eventName, args) {
-							$log.log('marker dragend');
-							$log.log(marker.getPosition().lat());
-							$log.log(marker.getPosition().lng());
-						}
-					}
-				}
-		 	});
-			//좌표 획득
-			$ionicLoading.hide();
-		 },function(error){
-		 	alert('code: '    + error.code    + '\n' +
-            'message: ' + error.message + '\n');
-		 });
-		}
-		//@@@ 현재 위치 획득
+		
+			var directions = new google.maps.DirectionsService(),
+		        clickTimer = null,
+		        markers = [];
+		    var myLatlng = new google.maps.LatLng(37.482,126.896);
+		    var mapOptions = {
+			      center: myLatlng,
+			      zoom: 15,
+			      draggableCursor: "crosshair",
+			      disableDoubleClickZoom: true
+			    };
+
+			var map = $scope.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+			//Marker + infowindow + angularjs compiled ng-click
+	        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+	        var compiled = $compile(contentString)($scope);
+
+	        var infowindow = new google.maps.InfoWindow({
+	          content: compiled[0]
+	        });
+
+		    var marker = new google.maps.Marker({
+		          position: myLatlng,
+		          map: map,
+		          title: 'Uluru (Ayers Rock)'
+		        });
+
+	        google.maps.event.addListener(marker, 'click', function() {
+	          infowindow.open(map,marker);
+	        });
+		      
+		    $scope.getCurrent = function(){
+				  if(!$scope.map) {
+			          return;
+			        }
+
+			        $scope.loading = $ionicLoading.show({
+			          content: 'Getting current location...',
+			          showBackdrop: true
+			        });
+
+			        navigator.geolocation.getCurrentPosition(function(pos) {
+			          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+			          var marker = new google.maps.Marker({
+				          position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+				          map: map,
+				          title: 'Uluru (Ayers Rock)'
+				        });
+			          $ionicLoading.hide();
+			        }, function(error) {
+			          alert('Unable to get location: ' + error.message);
+			        });
+			};
+		    
+
 	});
 });
--- www/js/utils/comm.js
+++ www/js/utils/comm.js
@@ -1,5 +1,5 @@
 function getRemoteServer(){
-	return "http://192.168.0.30:3000";
+	return "http://192.168.10.107:3000";
 }
 //오늘 날짜 yyyy-MM-dd 형식 return
 function getToday(){
