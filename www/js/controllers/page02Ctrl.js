67a18cb9a380e660d7415552e58a1cd02190feb0
huang <huang@huangMacBook-Pro.local>
2014-08-26 00:41:48 +0900

    201408260041

--- www/js/controllers/page02Ctrl.js
+++ www/js/controllers/page02Ctrl.js
@@ -1,25 +1,42 @@
 define(['angular'], function (angular) {//,'../../app/bower_components/lodash/dist/lodash.underscore.min','../../app/bower_components/angular-google-maps/dist/angular-google-maps.min'
 	return angular.module('starter.page02Ctrl', ['nsPopover'])
 	// A simple controller that fetches a list of data from a service
-	.controller('page02Ctrl', function($scope,$stateParams, $ionicLoading, $compile,$timeout,$log,$window) {
-		$scope.menuId = parseInt($stateParams.pid)-1;
+	.controller('page02Ctrl', function($scope,$stateParams, $ionicLoading, $state,$compile,$timeout,$rootScope,$state) {
+		    $scope.menuId = parseInt($stateParams.pid)-1;
+		    //console.log($rootScope.lag);
+		    $scope.page02Lat = "";
+		    $scope.page02Lng = "";
+		    if($rootScope.lat){
+		    	$scope.page02Lat = $rootScope.lat;
+		    	$scope.page02Lng = $rootScope.lng;
+		    }else{
+		    	$scope.page02Lat= 37.487;
+		    	$scope.page02Lng = 126.913;
+		    }
+		    
 			function initialize() {
 
 				 $scope.markers = [];
 				  var mapOptions = {
 				    zoom: 13,
-				    center: new google.maps.LatLng(40.7711329, -73.9741874),
+				    center: new google.maps.LatLng($scope.page02Lat, $scope.page02Lng),
 				    mapTypeId: google.maps.MapTypeId.ROADMAP
 				  }
-
 				 $scope.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+				 $scope.marker = new google.maps.Marker({
+							    position : new google.maps.LatLng($scope.page02Lat,$scope.page02Lng),
+							    title:"Hello World!"
+							});
+
+							// To add the marker to the map, call setMap();
+				$scope.marker.setMap($scope.map);
 			 }
 
-			initialize();
-			//google.maps.event.addDomListener(window, 'load', initialize);
+		   initialize();
 		   $scope.search = function(){
-		   		$window.location.href = "#/side/page02_1";
+		   		//$window.location.href = "#/side/page02_1";
+		   		$state.go('side.page02_1');
 		   };
 	});
 });
--- www/js/controllers/page02_1Ctrl.js
+++ www/js/controllers/page02_1Ctrl.js
@@ -1,83 +1,36 @@
 define(['angular'], function (angular) {//,'../../app/bower_components/lodash/dist/lodash.underscore.min','../../app/bower_components/angular-google-maps/dist/angular-google-maps.min'
 	return angular.module('starter.page02_1Ctrl', [])
 	// A simple controller that fetches a list of data from a service
-	.controller('page02_1Ctrl', function($scope,$stateParams, $ionicLoading, $compile,$timeout,$log) {
-		$scope.menuId = parseInt($stateParams.pid)-1;
-			function initialize() {
+	.controller('page02_1Ctrl', function($scope,$stateParams, $ionicLoading, $compile,$timeout,$state,$rootScope) {
+		//$scope.menuId   = parseInt($stateParams.pid)-1;
+		$scope.addressValue = "";
+		$scope.address = "";
+		$scope.addressResults = [];
+		//주소지 검색
+		$scope.changeAddress = function(addr){
+			$scope.geocoder = new google.maps.Geocoder();
+			 $scope.geocoder.geocode({  
+		        'address': addr 
+			    }, function(results, status){  
+			        if (status == google.maps.GeocoderStatus.OK) {  
+			           $scope.addressResults = results;
+			        } else {  
+			           // r.innerHTML = "검색 결과가 없습니다."+status;   
+			           //$scope.addressResults = [formatted_address:"결과 없습니다."];           
+			        }  
+			   });  
+		};
 
-				 $scope.markers = [];
-				  $scope.map = new google.maps.Map(document.getElementById('map-canvas'), {
-				    mapTypeId: google.maps.MapTypeId.ROADMAP
-				  });
+		//좌표를 얻어 지도에 보여주기
+		$scope.showMap = function(lat,lng){
+			$rootScope.lat = lat;
+			$rootScope.lng = lng;
+			//console.log($scope.lag+","+$scope.lng);
+			//$window.location.href = "#/side/page02";
+			//$state.go('side.page02', {groupId: -3});
+			$state.go('side.page02',{pid: 2});
+			//$window.history.go(-1);
+		}
 
-				  var defaultBounds = new google.maps.LatLngBounds(
-				      new google.maps.LatLng(-33.8902, 151.1759),
-				      new google.maps.LatLng(-33.8474, 151.2631));
-				  $scope.map.fitBounds(defaultBounds);
-
-				  // Create the search box and link it to the UI element.
-				  var input = /** @type {HTMLInputElement} */(
-				      document.getElementById('pac-input'));
-				  $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-				  $scope.searchBox = new google.maps.places.SearchBox(
-				    /** @type {HTMLInputElement} */(input));
-				  
-				  $scope.$watch('searchBox', function() {
-				    console.log($scope.searchBox);
-				  });
-				  // [START region_getplaces]
-				  // Listen for the event fired when the user selects an item from the
-				  // pick list. Retrieve the matching places for that item.
-				  google.maps.event.addListener($scope.searchBox, 'places_changed', function() {
-				    var places = $scope.searchBox.getPlaces();
-
-				    if (places.length == 0) {
-				      return;
-				    }
-				    for (var i = 0, marker; marker = markers[i]; i++) {
-				      marker.setMap(null);
-				    }
-
-				    // For each place, get the icon, place name, and location.
-				    $scope.markers = [];
-				    var bounds = new google.maps.LatLngBounds();
-				    for (var i = 0, place; place = places[i]; i++) {
-				      var image = {
-				        url: place.icon,
-				        size: new google.maps.Size(71, 71),
-				        origin: new google.maps.Point(0, 0),
-				        anchor: new google.maps.Point(17, 34),
-				        scaledSize: new google.maps.Size(25, 25)
-				      };
-
-				      // Create a marker for each place.
-				  $scope.marker = new google.maps.Marker({
-				        map: $scope.map,
-				       // icon: image,
-				        title: place.name,
-				        position: place.geometry.location
-				      });
-
-				      $scope.markers.push($scope.marker);
-
-				      bounds.extend(place.geometry.location);
-				    }
-
-				    $scope.map.fitBounds(bounds);
-				  });
-				  // [END region_getplaces]
-
-				  // Bias the SearchBox results towards places that are within the bounds of the
-				  // current map's viewport.
-				  google.maps.event.addListener($scope.map, 'bounds_changed', function() {
-				    var bounds = $scope.map.getBounds();
-				    $scope.searchBox.setBounds(bounds);
-				  });
-				}
-
-			initialize();
-			//google.maps.event.addDomListener(window, 'load', initialize);
-		   
 	});
 });
